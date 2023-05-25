c285029d455d734928e5c922bf788736ca437614
yma <yma@redhat.com>
2023-05-26 01:15:45 +0800

    Add tests for client models and moudles

--- models/core-java/src/main/java/org/commonjava/indy/core/expire/Expiration.java
+++ models/core-java/src/main/java/org/commonjava/indy/core/expire/Expiration.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.commonjava.indy.expire;
+package org.commonjava.indy.core.expire;
 
 import java.util.Date;
 
